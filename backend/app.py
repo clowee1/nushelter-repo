688ca976b7d9a5edcac65054efb99dbfec657fe0
clowee1 <e1524094@u.nus.edu>
2026-06-01 01:12:59 +0800

    supabase installment

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,26 @@
 from flask import Flask, request
 from flask_cors import CORS
+from dotenv import load_dotenv
+import os
+from supabase import create_client
 
 app = Flask(__name__)
 CORS(app)
+load_dotenv()
 
-users = []
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+supabase = create_client(
+    SUPABASE_URL,
+    SUPABASE_KEY
+)
+
+@app.route("/test-db")
+def test_db():
+
+    result = supabase.table("users").select("*").execute()
+
+    return result.data
 
 @app.route("/")
 def home():
@@ -15,63 +31,71 @@ def home():
 @app.route("/register", methods=["POST"])
 def register():
 
+    print("Register route called")
+
     data = request.get_json()
+    print(data)
 
     name = data["name"]
     email = data["email"]
     password = data["password"]
 
-    for user in users:
-        if user["email"] == email:
-            return {
-                "message" : "Email already exists"
-            }, 400
-        
-    new_user = {
-        "name" : name,
-        "email" : email,
-        "password" : password
-    }
+    existing_user = (
+        supabase.table("users")
+        .select("*")
+        .eq("email", email)
+        .execute()
+    )
 
-    users.append(new_user) 
+    if existing_user.data:
+        return {
+            "message": "Email already exists"
+        }, 400
     
+    supabase.table("users").insert({
+        "name": name,
+        "email": email,
+        "password": password
+    }).execute()
+
 
     return {
-        "message": "User registered successfully",
-        "user": new_user
+        "message": "User registered successfully"
     }
 
 @app.route("/login", methods=["POST"])
-def logIn():
+def login():
 
     data = request.get_json()
 
     email = data["email"]
     password = data["password"]
 
-    for user in users:
-        if user["email"] == email:
-            if user["password"] == password:
-                return {
-                    "message": "Successful user login",
-                    "user": {
-                        "name": user["name"],
-                        "email": user["email"]
-                    }
-                }
-            else:
-                return {
-                    "message": "Incorrect password"
-                }, 401 
-    return {
-        "message": "User not found"
-    }, 404
+    result = (supabase.table("users")
+                     .select("*")
+                     .eq("email", email)
+                     .execute()
+    )
+
+    if not result.data:
+        return {
+            "message": "User not found"
+        }, 404
     
-@app.route("/users")
-def getUsers():
+    user = result.data[0]
+
+    if user["password"] != password:
+        return {
+            "message": "Incorrect password"
+        }, 401
     return {
-        "users" : users
+        "message": "Login successful",
+        "user": {
+            "name": user["name"],
+            "email": user["email"]
+        }
     }
+
 
 if __name__ == "__main__":
     app.run(debug=True)
